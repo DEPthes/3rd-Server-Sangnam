2c82a4c0db1693af56174cadf5820a0762328a67
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2024-05-28 23:12:44 +0900

    feat: 6주차 과제 제출 - 동시성 처리(2)

---
+++ 5.concurrency-2/concurrencyStudy2/src/hashtable.java
@@ -0,0 +1,11 @@
+public class hashtable<K,V>
+        extends Dictionary<K,V>
+        implements Map<K,V>, Cloneable, java.io.Serializable {
+
+    public synchronized int size() { }
+
+    @SuppressWarnings("unchecked")
+    public synchronized V get(Object key) { }
+
+    public synchronized V put(K key, V value) { }
+}
